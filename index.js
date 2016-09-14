d1e58b63cc345edf6cf7366fe3558e768b82ba29
Kash Cummings <kashleec@gmail.com>
2016-09-14 16:19:47 -0500

    change to bot framework

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var express = require('express');
-var mongoose = require('mongoose');
+// var express = require('express');
+// var mongoose = require('mongoose');
 
-var PORT = process.env.PORT || 9000;
-var app = express();
+// var PORT = process.env.PORT || 9000;
+// var app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL);
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGO_URL);
 
-app.use('', require('./api'));
+// app.use('', require('./api'));
 
-app.listen(PORT);
+// app.listen(PORT);
+
+var restify = require('restify');
+var builder = require('botbuilder');
+var config = require('./config');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(config.port, function () {});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: config.microsoft.appId,
+    appPassword: config.microsoft.appPassword
+});
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
